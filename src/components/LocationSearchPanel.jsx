5a5b388ddcc36f34a0367c2e71f983055acb64c8
Sayan-ghs <sg865186@gmail.com>
2025-07-13 23:09:58 +0530

    update theHome page

--- src/components/LocationSearchPanel.jsx
+++ src/components/LocationSearchPanel.jsx
@@ -3,7 +3,33 @@ import React from 'react'
 const LocationSearchPanel = () => {
   return (
     <div>
-      Hii location
+      <div className='flex gap-4 active:border-2 p-3 my-2 rounded- items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 w-16 flex items-center justify-center rounded-full'><i className="ri-map-pin-line"></i></h2>
+        <h4 className='font-medium'>
+          84,Vidyasagar Sarani, Ghoshpara, Bally, Howrah
+        </h4>
+      </div>
+
+      <div className='flex gap-4 active:border-2 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 w-16 flex items-center justify-center rounded-full'><i className="ri-map-pin-line"></i></h2>
+        <h4 className='font-medium'>
+          84,Vidyasagar Sarani, Ghoshpara, Bally, Howrah
+        </h4>
+      </div>
+
+      <div className='flex gap-4 active:border-2 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 w-16 flex items-center justify-center rounded-full'><i className="ri-map-pin-line"></i></h2>
+        <h4 className='font-medium'>
+          84,Vidyasagar Sarani, Ghoshpara, Bally, Howrah
+        </h4>
+      </div>
+
+      <div className='flex gap-4 active:border-2 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 w-16 flex items-center justify-center rounded-full'><i className="ri-map-pin-line"></i></h2>
+        <h4 className='font-medium'>
+          84,Vidyasagar Sarani, Ghoshpara, Bally, Howrah
+        </h4>
+      </div>
     </div>
   )
 }
